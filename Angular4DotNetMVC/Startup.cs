e953517268725aca874e09b71402e433a98b830e
vdab-pierre <pierre.colin@vdab.be>
2016-10-20 15:24:50 +0200

    Add project files.

---
+++ Angular4DotNetMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Angular4DotNetMVC.Startup))]
+namespace Angular4DotNetMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
